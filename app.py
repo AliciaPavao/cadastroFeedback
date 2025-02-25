80e72e751d2c4060fe0f7aa18ff382c1ca9d3ad3
AliciaPavao <aliciapavao369@gmail.com>
2025-02-25 11:25:41 -0300

    Criação de uma rota + css + junção com banco de dados

--- app.py
+++ app.py
@@ -1,8 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import datetime
+import mysql.connector
 app = Flask(__name__)
 
 @app.route("/")
 def pagina_principal():
-    return "Página principal"
+    return render_template("pagPrincipal.html")
+
+@app.route("/post/mensagem", methods=["POST"])
+def cadastrarComentarios():
+    usuario = request.form.get("usuario")
+    mensagem = request.form.get("mensagem")
+    data_hora = datetime.datetime.today()
+    
+    conexao = mysql.connector.connect(host = "localhost",
+                            port = 3306,
+                            user = "root",
+                            password = "root",
+                            database = "dbComentarios")
+    
+    cursor = conexao.cursor()
+
+    # Criando o sql que será executado
+    sql = """INSERT INTO tbcomentarios
+            (nome, data_hora, comentario)
+            VALUES
+                (%s,%s,%s)"""
+    valores=(usuario, data_hora, mensagem)
+
+    # Executando o comando sql
+    cursor.execute(sql, valores)
+
+    # Confirmo a alteração
+    conexao.commit()
+
+    # Fecho a conexao com o banco
+    cursor.close()
+    conexao.close() 
+
+    return render_template("pagPrincipal.html")
+
+# Cadastrando as coisas no banco de dados
 
 app.run(debug = True)
