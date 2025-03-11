da15fb427d36cb01d2579bd01b641c53ad8f9e4d
AliciaPavao <aliciapavao369@gmail.com>
2025-03-11 11:19:38 -0300

    def para armazenar e mostrar comentarios e css

--- model/control_mensagem.py
+++ model/control_mensagem.py
@@ -26,3 +26,28 @@ class Mensagem:
         # Fecho a conexao com o banco
         cursor.close()
         conexao.close() 
+
+    def recuperar_mensagens():
+        # Criando a conexão
+        conexao = Conexao.criar_conexao()
+
+        # O cursor será responsavel por manipular o banco de dados
+        cursor = conexao.cursor(dictionary = True)
+
+        # Criando o SQL que será executado
+        sql = """select nome AS usuario, 
+                        comentario AS mensagem,
+                        data_hora
+                        from tbcomentarios"""
+        
+        # Executando o comando sql
+        cursor.execute(sql)
+
+        # Recuperando os dados e guardando em uma variavel
+        resultado = cursor.fetchall()
+
+        # Fecho a conexão com o banco
+        conexao.close()
+
+        return resultado
+    
